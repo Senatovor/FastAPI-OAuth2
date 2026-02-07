bf8e085b71bc8e24f7cfbd8d6d88493c47c0ecd2
Орлов Арсений <orlow.sen@gmail.com>
2026-02-07 19:00:48 +0300

    Добавление логики scopes

--- src/auth_api/dependencies.py
+++ src/auth_api/dependencies.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
 from typing import Annotated
 from loguru import logger
 from jose import JWTError
@@ -9,8 +9,12 @@ from .models import User
 from ..database.session import DbSessionDepends
 from ..database.executer import sql_manager
 from .handler import AuthHandler
+from .scopes_dict import scopes
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth_api/token")
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/auth_api/token",
+    scopes=scopes,
+)
 
 NotAuthException = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
@@ -20,12 +24,14 @@ NotAuthException = HTTPException(
 
 
 async def get_current_user(
+        security_scopes: SecurityScopes,
         token: Annotated[str, Depends(oauth2_scheme)],
         db_session: DbSessionDepends()
 ) -> User:
     """Получает текущего пользователя по JWT токену.
 
     Args:
+        security_scopes(SecurityScopes): Права
         token(str): JWT токен
         db_session(AsyncSession): Сессия базы данных
 
@@ -40,12 +46,27 @@ async def get_current_user(
         email = payload.get('sub')
         if email is None:
             raise NotAuthException
+
+        scope: str = payload.get("scope", "")
+        token_scopes = scope.split(" ")
+
+        user = await sql_manager(
+            select(User).where(User.email == email)
+        ).scalar_one_or_none(db_session)
+
+        if user is None:
+            logger.error('Не найден пользователь')
+            raise NotAuthException
+
+        for scope in security_scopes.scopes:
+            if scope not in token_scopes:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Not enough permissions",
+                    headers={"WWW-Authenticate": f'Bearer scope="{security_scopes.scope_str}"'},
+                )
     except JWTError as e:
         logger.error(f'Во время декодирования произошла ошибка: {e}')
-        raise NotAuthException
-    user = await sql_manager(select(User).where(User.email == email)).scalar_one_or_none(db_session)
-    if user is None:
-        logger.error('Не найден пользователь')
         raise NotAuthException
 
     return user
